77d32f1d9627739d6710de902b73e20cbd5fde70
goutjeremiah <goutjeremiah@gmail.com>
2025-08-10 12:40:00 +0100

    Delete src directory

--- src/src/utils/formatNumber.jsx
+++
@@ -1,7 +0,0 @@
-export const formatNumber = (num) => {
-    return new Intl.NumberFormat('en-US').format(num);
-};
-
-export const formatCurrency = (num) => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(num);
-};
